96ad85c8c6d807ae7d7cfc49c54f5766176dac5e
SaiVaraprasad Medapati <varaprasadreddy9676@gmail.com>
2026-03-11 20:44:14 +0530

    Remove backup prod source snapshot

--- backend/src/bkp prod/utils/time.js
+++
@@ -1,7 +0,0 @@
-function toIso(date) {
-  if (!date) return undefined;
-  if (date instanceof Date) return date.toISOString();
-  return new Date(date).toISOString();
-}
-
-module.exports = { toIso };
